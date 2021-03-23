a2128a1065a612eb4e1167c5f88cc3c2a12c0db8
zulc22 <zulc22.db@gmail.com>
2021-03-23 06:03:59 +0000

    WHOOPS! i forgot to TEST my CODE before COMMITING IT

--- static/js/homepage.js
+++ static/js/homepage.js
@@ -1,44 +1,59 @@
-console.log("hello")
+$(()=>{ // defer
 
-$('.progressbar').hide();
+var uploadform = $("#uform");
+var progressbar = $(".progressbar");
+var bar = $(".bar");
+var status = $('#status');
+var postto = "/api/video";
 
+progressbar.hide();
+
+// when you press submit
 $("#uform").on('submit', (e) => {
+
+    // i have to both of these for it to not redirect (i want it to redirect if JS is off)
     e.stopImmediatePropagation();
     e.preventDefault();
+    
+    // good luck understanding this shit. it's mostly copy pasted.
     $.ajax({
         xhr: () => {
             var xhr = new window.XMLHttpRequest();
             xhr.upload.addEventListener("progress", (evt) => {
                 if (evt.lengthComputable) {
                     var percentComplete = Math.floor((evt.loaded / evt.total) * 100);
-                    $(".bar").width(percentComplete + '%');
-                    $(".bar").html(percentComplete + '%');
+                    bar.width(percentComplete + '%');
+                    bar.text(percentComplete + '%');
                 }
-                $('#status').html('uploading...');
             }, false);
             return xhr;
         },
+        // if you can't understand this part you shouldn't be reading this
         type: 'POST',
-        url: '/api/video',
-        data: new FormData($('#uform')[0]),
+        url: postto,
+        // afaik this only works for POST. don't care enough to check.
+        data: new FormData(uploadform[0]),
+        // no idea why this shit is 'false'.
         contentType: false,
         cache: false,
         processData: false,
+        // right before data starts to be sent
         beforeSend: () => {
-            $('#uform').hide();
-            $('.progressbar').show();
-            console.log('beforesend');
-            $(".bar").width('0%');
-            $('#status').html('hold on');
+            uploadform.hide();
+            progressbar.show();
+            bar.width('0%');
+            status.text('uploading...');
         },
+        // when the form gets a non-200 code probably
         error: () => {
-            console.log('err')
-            $('#status').html('it didnt fucking work');
+            status.text('it didnt work');
         },
+        // when the form succeeds. resp is a string of what the server sent back 
         success: (resp) => {
-            console.log('succ')
-            $('#uform')[0].reset();
-            $('#status').html(`it fuckin g worked<br>response: ${resp}`);
+            uploadform[0].reset();
+            status.html(`it worked<br>response: ${resp}`);
         }
     });
 });
+
+}); // defer
